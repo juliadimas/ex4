7773879595b9c2467ff24acc6df5b1d502d3e4db
BeaAmaral <beatrizamaralsilva02@gmail.com>
2019-03-27 10:28:27 -0300

    adicionando aluno lp

---
+++ exercicios/Novo1.pas
@@ -0,0 +1,15 @@
+Program Pzim ;
+var a,b:integer;
+m:real;
+Begin
+  
+  write('digite um numero -> ');
+  read (a);
+  writeln('digite um outro numero -> ');
+  read (b);
+  
+   m:=(a+b)/2;
+  
+   writeln ('A média é -> ',m);
+  
+End.
---
+++ exercicios/Novo2.pas
@@ -0,0 +1,17 @@
+Program Pzim ;
+var
+ca,co,h,x,r,s:real;
+Begin
+  
+  s:= 170+150;
+  ca:= 170*170;
+  co:= 150*150;
+  h:=  co+ca;
+  x:=  sqrt(h);
+  r:=  s-x;
+  
+  writeln(x);
+  writeln(r);
+  
+  
+End.
